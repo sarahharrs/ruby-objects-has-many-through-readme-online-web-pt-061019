2d2bd45afd8bd068217a3d721552b31d31e642d4
Sarah Harrs <sarahharrs@gmail.com>
2019-07-29 09:47:55 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,2 +1,20 @@
 require_relative './lib/customer.rb'
 require_relative './lib/waiter.rb'
+class Meal
+
+  attr_accessor :waiter, :customer, :total, :tip
+
+  @@all = []
+
+  def initialize(waiter, customer, total, tip=0)
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end
